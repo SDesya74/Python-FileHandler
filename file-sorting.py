8b5bd22aeb96b5c820923ba2423509ee1ff8d211
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2020-12-19 11:44:23 +0300

    Change extension getting

--- file-sorting.py
+++ file-sorting.py
@@ -2,19 +2,20 @@
 
 from watchdog.observers import Observer
 from os import listdir, rename, environ, makedirs
-from os.path import isfile, join, splitext, exists, dirname
+from os.path import join, splitext, exists, dirname
 import time
 from watchdog.events import FileSystemEventHandler
 
 
 class FileHandler(FileSystemEventHandler):
     def on_any_event(self, event):  # on file modified
+        print("hello")
         for name in listdir(main_folder):
             src_file = join(main_folder, name)
 
             ext = splitext(name)[-1][
-                  1::]  # [-1] is the last element of list, [1::] removes first character of extension (a dot symbol)
-            target = folder_by_extension.get(ext, str.capitalize(ext))
+                  1::]  # [-1] is the last element of list, [1::] removes first character of extension (a dot symbol# )
+            target = get_folder_by_extension(ext)
             dst_file = join(main_folder, target, name)
 
             if not exists(dirname(dst_file)):
@@ -28,14 +29,23 @@ class FileHandler(FileSystemEventHandler):
                     time.sleep(5)
 
 
-# environ["USERPROFILE"] <- path to user folder in Windows
-main_folder = join(environ["USERPROFILE"], "Desktop", "Python", "003-file-sorting-by-extension")
-folder_by_extension = {
-    "jpg": "Photos",
-    "mp3": "Music",
-    "mp4": "Video",
-    "py": "Python Scripts"
-}
+folder_by_extension = [
+    ("Photos", ["jpg", "png"]),
+    ("Documents", ["txt", "docx"])
+]
+
+
+def get_folder_by_extension(ext):
+    for row in folder_by_extension:
+        folder_name = row[0]
+        extensions = row[1]
+        if ext in extensions:
+            return folder_name
+
+    return str.capitalize(ext)
+
+
+main_folder = "Sort"
 
 handler = FileHandler()
 observer = Observer()
